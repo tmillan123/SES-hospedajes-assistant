f04a7417e342dfb4c1bd723139c229e1a2cff94a
tmillan123 <tmf33uk@yahoo.com>
2026-07-18 15:32:14 +0200

    Update guest.py

--- src/guest.py
+++ src/guest.py
@@ -1,1 +1,18 @@
+from dataclasses import dataclass
+from datetime import date
+from typing import Optional
 
+
+@dataclass
+class Guest:
+    first_name: str
+    last_name: str
+    date_of_birth: date
+    document_type: str
+    document_number: str
+    nationality: str
+
+    second_last_name: Optional[str] = None
+    phone_number: Optional[str] = None
+    email: Optional[str] = None
+    address: Optional[str] = None
