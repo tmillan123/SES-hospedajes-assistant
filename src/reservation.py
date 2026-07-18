8181f2d890242a54086e84b2d8f6cc8badc136bc
tmillan123 <tmf33uk@yahoo.com>
2026-07-18 16:41:03 +0200

    Add Reservation dataclass with attributes

--- src/reservation.py
+++ src/reservation.py
@@ -1,1 +1,13 @@
+from dataclasses import dataclass, field
+from datetime import date
+from typing import List
 
+from guest import Guest
+
+
+@dataclass
+class Reservation:
+    reservation_number: str
+    check_in: date
+    check_out: date
+    guests: List[Guest] = field(default_factory=list)
